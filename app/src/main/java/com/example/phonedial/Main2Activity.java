b4a65f42f4c92419956f297ce5e856e2f7eb1b58
missTinuade <estherjolugba@yahoo.com>
2020-02-04 11:57:24 +0000

    app crash fixed

--- app/src/main/java/com/example/phonedial/Main2Activity.java
+++ app/src/main/java/com/example/phonedial/Main2Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,9 +31,10 @@ public class Main2Activity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri number=Uri.parse(text);
+                Uri number=Uri.parse("tel:"+text);
                 Intent callIntent =new Intent(Intent.ACTION_DIAL,number);
                 startActivity(callIntent);
+
 
             }
         });
--- app/src/main/java/com/example/phonedial/MainActivity.java
+++ app/src/main/java/com/example/phonedial/MainActivity.java
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
